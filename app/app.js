9383543d36e29c801285c19220ae00794a323973
andela-sadelokiki <susan.adelokiki@andela.co>
2015-04-13 23:27:03 +0100

    Version 1.0

--- app/app.js
+++ app/app.js
@@ -1,12 +1,57 @@
-'use strict';
+var app = angular.module('ToDo',[]);
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+app.controller('ToDoCtrl', ['$scope', 
+  function($scope) {
+
+    $scope.today = new Date();
+    $scope.newTaskDate = '';
+
+    $scope.categories = [
+    {name: 'Personal'}, 
+    {name:'work'},
+    {name:'other'}
+    ];
+
+    $scope.newTaskCategory = $scope.categories;
+
+    $scope.todoList = [
+      {text:'Watch Furious',done: false,  date:$scope.newTaskDate},
+      {text:'Build a Todo App', done:false}
+    ];
+
+   
+
+     $scope.addTask = function(){
+      //console.log('newTask: ', $scope.newTask);
+      //$scope.newTask.trim();
+        if($scope.newTaskDate === null || $scope.newTaskDate === '' )
+           {
+              alert("input due date!");
+           }
+        else{
+          if($scope.newTask !== "")
+            {
+               $scope.todoList.push({text:$scope.newTask , done: false , date:$scope.newTaskDate , category: $scope.newTaskCategory.name});
+              $scope.newTask = "";
+            }
+    } 
+     };
+
+
+     $scope.delete= function(index){
+      //$scope.todoList = _.filter($scope.todoList, //function(todo){
+      //return !todo.done;
+    //});
+       var completedTask = $scope.todoList;
+        $scope.todoList = [];
+        angular.forEach(completedTask, function (todo) {
+            if (!todo.done) {
+                $scope.todoList.push(todo);
+            }
+        });
+        
+      
+  };
+
+   
+  }]);
